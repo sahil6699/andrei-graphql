39e35f65ba2699c002b29e8078db4075b37fe6d5
sahil6 <sahil@anubudh.com>
2022-08-23 11:30:51 +0530

    modularizing our code4
    we can modularise the root defalut values too by making the order.model.js and prodcuts.models.js
    files, here we

--- server.js
+++ server.js
@@ -1,52 +1,27 @@
 const path = require('path')
 const express = require('express')
-const { buildSchema } = require('graphql')
+
+// const { buildSchema } = require('graphql')
 const { graphqlHTTP } = require('express-graphql')
 
-const { loadFileSync } = require('@graphql-tools/load-files')
-const makeExecutableSchema = require('@graphql-tools/schema')
+const { loadFilesSync } = require('@graphql-tools/load-files')
+const { makeExecutableSchema } = require('@graphql-tools/schema')
 
-const typesArray = loadFileSync(path.join(__dirname, '**/*.graphql'))
+const typesArray = loadFilesSync(path.join(__dirname, '**/*.graphql'))
 
-const schemaText = `type Query {
-		products: [Product]
-		orders: [Order]
-	}
-`
+// const schemaText = `type Query {
+// 		products: [Product]
+// 		orders: [Order]
+// 	}
+// `
 
 const schema = makeExecutableSchema({
   typeDefs: typesArray,
 })
 
 const root = {
-  products: [
-    {
-      id: 'redshoe',
-      description: 'Red Shoe',
-      price: 42.12,
-    },
-    {
-      id: 'bluejean',
-      description: 'Blue Jeans',
-      price: 55.55,
-    },
-  ],
-  orders: [
-    {
-      date: '2005-05-05',
-      subtotal: 90.22,
-      items: [
-        {
-          product: {
-            id: 'redshoe',
-            description: 'Old Red Shoe',
-            price: 45.11,
-          },
-          quantity: 2,
-        },
-      ],
-    },
-  ],
+  products: require('./products/products.model.js'),
+  orders: require('./orders/orders.model.js'),
 }
 
 const app = express()
