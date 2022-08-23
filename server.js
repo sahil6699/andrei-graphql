b62eafa04035bf6c0fef62267300150416e62cd7
sahil6 <sahil@anubudh.com>
2022-08-23 10:56:47 +0530

    modularizing our code3
    combine the files using the
    @graphql-tools/load-files
    
    1. import @graphql-tools/load-files as loadFileSync
    2. make a typeArray vairable and put this value in it -loadFileSync(path.join(__dirname, '**/*.graphql'))
    3. now instead of schemaText pass this variable to typedefs

--- server.js
+++ server.js
@@ -1,88 +1,65 @@
-const express = require("express")
-const {buildSchema }= require("graphql")
-const {graphqlHTTP} = require("express-graphql")
+const path = require('path')
+const express = require('express')
+const { buildSchema } = require('graphql')
+const { graphqlHTTP } = require('express-graphql')
 
-const makeExecutableSchema = require("@graphql-tools/schema")
+const { loadFileSync } = require('@graphql-tools/load-files')
+const makeExecutableSchema = require('@graphql-tools/schema')
+
+const typesArray = loadFileSync(path.join(__dirname, '**/*.graphql'))
 
 const schemaText = `type Query {
 		products: [Product]
 		orders: [Order]
 	}
+`
 
-	type Product {
-		id: ID!
-		description: String!
-		reviews: [Review]
-		price: Float!
-	}
-
-	type Review {
-		rating: Int!
-		comment: String
-	}
-
-	type Order {
-		date: String
-		subtotal: Float!
-		items: [OrderItem]
-	}
-
-	type OrderItem {
-		product: Product!
-		quantity: Int!
-	}`
-
-
-const schema = makeExecutableSchema( {
-	typeDefs: [schemaText]
+const schema = makeExecutableSchema({
+  typeDefs: typesArray,
 })
 
-const schema = buildSchema(`
-	
-`)
-
-
 const root = {
-	products: [
-	 {
-		id: 'redshoe',
-		description: 'Red Shoe',
-		price: 42.12,
-	 }, 
-	 {
-	 	id: 'bluejean',
-	 	description:'Blue Jeans',
-	 	price: 55.55,
-	 }
-	],
-	orders: [
-		{
-			date: '2005-05-05',
-			subtotal: 90.22,
-			items: [
-				{
-					product: {
-						id: 'redshoe',
-						description: 'Old Red Shoe',
-						price: 45.11,
-					},
-					quantity: 2,
-				}
-			]
-		}
-	]
+  products: [
+    {
+      id: 'redshoe',
+      description: 'Red Shoe',
+      price: 42.12,
+    },
+    {
+      id: 'bluejean',
+      description: 'Blue Jeans',
+      price: 55.55,
+    },
+  ],
+  orders: [
+    {
+      date: '2005-05-05',
+      subtotal: 90.22,
+      items: [
+        {
+          product: {
+            id: 'redshoe',
+            description: 'Old Red Shoe',
+            price: 45.11,
+          },
+          quantity: 2,
+        },
+      ],
+    },
+  ],
 }
 
 const app = express()
 
-app.use('/graphql',graphqlHTTP({
-	schema: schema,
-	rootValue: root,
-	graphiql: true
-}))
-
-
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+)
 
 app.listen(3000, () => {
-	console.log("Running our graphql server")
+  console.log('Running our graphql server')
 })
